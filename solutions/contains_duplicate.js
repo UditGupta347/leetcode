cb78aa0fe128899924be7fd181caead445d5eaf1
Udit Gupta <uditgupta222568@gmail.com>
2026-01-24 22:04:06 +0530

    Update solution for contains_duplicate

--- solutions/contains_duplicate.js
+++ solutions/contains_duplicate.js
@@ -1,7 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         for(int i=0;i<nums.length;i++){
-            if(nums[i])
+            if(nums[i] == )
         }
     }
 }
