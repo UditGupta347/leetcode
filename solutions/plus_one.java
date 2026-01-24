a7333045f73a6a766d455241c68c8df98d65af74
Udit Gupta <uditgupta222568@gmail.com>
2026-01-24 22:28:06 +0530

    Update solution for plus_one

--- solutions/plus_one.java
+++ solutions/plus_one.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int last = digits.length-1;
         for(int i=0;i<digits.length;i++){
-
+            int new =
         }
     }
 }
