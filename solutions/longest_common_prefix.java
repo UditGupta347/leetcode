822bb4d6defe6ffa13935c9e13f4fc7e9cc7ec66
Udit Gupta <uditgupta222568@gmail.com>
2026-01-24 22:15:03 +0530

    Update solution for longest_common_prefix

--- solutions/longest_common_prefix.java
+++ solutions/longest_common_prefix.java
@@ -2,5 +2,7 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         Arrays.sort(strs);
         String s1 = strs[0];
+        String s2  = strs.[strs.length-1];
+        int index
     }
 }
