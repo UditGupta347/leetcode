ca4b04adccad048b7955e1e51b1f8d10e5a958ae
Udit Gupta <uditgupta222568@gmail.com>
2026-01-24 22:20:23 +0530

    Update solution for search_insert_position

--- solutions/search_insert_position.java
+++ solutions/search_insert_position.java
@@ -4,7 +4,7 @@ class Solution {
 
         for(int i=0;i<nums.length();i++){
             if(nums[i]==target){
-                return(i)
+                return(i);
             }
         }
     }
