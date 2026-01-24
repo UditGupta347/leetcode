b7943bb9a3d4c7527e0a267ef56379a7269c7ed1
Udit Gupta <uditgupta222568@gmail.com>
2026-01-24 22:13:10 +0530

    Update solution for median_of_two_sorted_arrays

--- solutions/median_of_two_sorted_arrays.java
+++ solutions/median_of_two_sorted_arrays.java
@@ -1,8 +1,5 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        for(int i=0;i<strs.length;i++){
-            if
-        }
     }
 }
