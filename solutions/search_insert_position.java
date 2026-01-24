1b8b86cd009b2088210f67ca254db65cc124c66f
Udit Gupta <uditgupta222568@gmail.com>
2026-01-24 22:22:30 +0530

    Update solution for search_insert_position

--- solutions/search_insert_position.java
+++ solutions/search_insert_position.java
@@ -2,9 +2,9 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         Arrays.sort(nums);
 
-        for(int i=0;i<nums.length();i++){
-            if(target<nums[i]){
+        for(int i=0;i<nums.length;i++){
+            if(target<=nums[i]){
                 return(i);
             }
         }
-    }
+        return nums.length;
