fcb0c713caabf2ff1e76c6592b24b51293d23a2e
Udit Gupta <uditgupta222568@gmail.com>
2026-01-26 02:09:45 +0530

    Update solution for length_of_last_word

--- solutions/length_of_last_word.java
+++ solutions/length_of_last_word.java
@@ -1,12 +1,10 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String str = " ";
-        for(int i = s.length()-1 ;i>=0 ;  i--){
-            if(s.charAt[i] = " "){
-                s.push(str);
-                break;
-            }
-        }
-        return str.length();
-    }
-}
+        String str = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                if (str.length() > 0) {
+                    break;
+                }
+            } else {
+                str = str + s.charAt(i);
