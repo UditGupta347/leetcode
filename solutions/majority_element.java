6000521958b91f152672584149ed99ad898da601
Udit Gupta <uditgupta222568@gmail.com>
2026-01-26 22:18:39 +0530

    Update solution for majority_element

--- solutions/majority_element.java
+++ solutions/majority_element.java
@@ -2,7 +2,9 @@ class Solution {
     public int majorityElement(int[] nums) {
 
         for(int i=0;i<nums.length;i++){
-            if(nums.charAt(i)>nums.charAt(i+1))
+            if(nums.charAt(i)>nums.charAt(i+1)){
+                i++;
+            }
         }
+        return nums.charAt[]
     }
-}
