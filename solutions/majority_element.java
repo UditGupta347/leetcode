b5fd14e5fa0909dfbac5b83f358c5528747209aa
Udit Gupta <uditgupta222568@gmail.com>
2026-01-26 22:20:10 +0530

    Update solution for majority_element

--- solutions/majority_element.java
+++ solutions/majority_element.java
@@ -1,10 +1,18 @@
-class Solution {
-    public int majorityElement(int[] nums) {
+        int count = 0;        int count = 0;
+        int candidate = 0;        int candidate = 0;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums.charAt(i)>nums.charAt(i+1)){
-                i++;
-            }
-        }
-        return nums.charAt[i];
-    }
+        for(int num : nums) {        for(int num : nums) {
+            if(count == 0) {            if(count == 0) {
+                candidate = num;                candidate = num;
+            }            }
+
+            if(num == candidate) {            if(num == candidate) {
+                count++;                count++;
+            } else {            } else {
+                count--;                count--;
+        }        }
+
+        return candidate;        return candidate;
+    }    }
+class Solution {class Solution {
+    public int majorityElement(int[] nums) {    public int majorityElement(int[] nums) {
