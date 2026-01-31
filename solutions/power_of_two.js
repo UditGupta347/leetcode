a6a57a11bd4650b4d538da9a5d081cc2ea717219
Udit Gupta <uditgupta222568@gmail.com>
2026-01-31 20:12:45 +0530

    Add solution for power_of_two

---
+++ solutions/power_of_two.js
@@ -0,0 +1,12 @@
+/*
+ * Approach/Algorithm:
+ * Check if n is greater than 0 and if n & (n - 1) equals 0. This checks if n is a power of two without using loops or recursion.
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+    }
+}
