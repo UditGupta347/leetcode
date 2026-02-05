4ce28f4def929e944be74020ec90a924ccc84936
Udit Gupta <uditgupta222568@gmail.com>
2026-02-05 22:28:12 +0530

    Add solution for merge_two_sorted_lists

---
+++ solutions/merge_two_sorted_lists.php
@@ -0,0 +1,19 @@
+/*
+ * Approach/Algorithm:
+ * This solution uses a dummy node to simplify merging and avoid edge cases. It iterates through both lists,
+ * comparing the current nodes and adding the smaller node to the merged list.
+ *
+ * Time Complexity: O(n + m), where n and m are the lengths of list1 and list2, respectively.
+ * Space Complexity: O(1), as we are only using a few extra pointers regardless of input size.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
