eb10d0042dc50a73efc6a0057ed96d9dc1fcc2bf
Udit Gupta <uditgupta222568@gmail.com>
2026-02-05 18:30:40 +0530

    Update solution for missing_number

--- solutions/missing_number.java
+++ solutions/missing_number.java
@@ -3,8 +3,8 @@ class Solution {
     public int missingNumber(int[] nums) {
         /*
          * Approach/Algorithm: Use the Gauss formula to calculate the sum of the first n natural numbers and subtract the sum of the given array to find the missing number.
-         * Time Complexity: O(n)
-         * Space Complexity: O(1)
+         * Time Complexity: O(n), where n is the length of the input array.
+         * Space Complexity: O(1), using only a constant amount of extra space.
          */
     }
 }
