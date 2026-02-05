d7e210d59fe49294756f571ac667e692ef2f2b2a
Udit Gupta <uditgupta222568@gmail.com>
2026-02-05 18:31:15 +0530

    Update solution for missing_number

--- solutions/missing_number.java
+++ solutions/missing_number.java
@@ -1,22 +1,15 @@
 /*
-class Solution {
-    public int missingNumber(int[] nums) {
-        int missing = nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            missing ^= i ^ nums[i];
-        }
-        return missing;
-    }
-}
-
-/*
- * Approach/Algorithm: This solution uses the XOR operation to find the missing number. It leverages the property of XOR where a number XORed with itself is 0.
+ * Approach/Algorithm: This solution uses the Gauss' formula to calculate the sum of the first n natural numbers and then subtracts the sum of the given array to find the missing number.
  * Time Complexity: O(n)
  * Space Complexity: O(1)
  */
 
 class Solution {
     public int missingNumber(int[] nums) {
-        int
+        int res = nums.length;
+
+        for(int i=0;i<nums.length;i++){
+            res+=
+        }
     }
 }
