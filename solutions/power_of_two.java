8f7717e2df0d0e60d109d67667c3ece023983f70
Udit Gupta <uditgupta222568@gmail.com>
2026-02-05 22:50:21 +0530

    Update solution for power_of_two

--- solutions/power_of_two.java
+++ solutions/power_of_two.java
@@ -1,8 +1,8 @@
 /*
  * Approach/Algorithm:
  * This solution checks if a number is a power of two by continuously dividing the number by 2 until it is no longer even. If the resulting number is 1, it returns true; otherwise, it returns false.
- * Time Complexity: O(log n), where n is the input number, due to the repeated division by 2.
- * Space Complexity: O(1), as no additional space is used that scales with input size.
+ * Time Complexity: O(log n), where n is the input number, as it halves the number in each iteration.
+ * Space Complexity: O(1), as it uses a constant amount of space.
  */
 
 class Solution {class Solution {
