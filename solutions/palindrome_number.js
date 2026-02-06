f61f56bd0da9dc56fb0ac70952c260a0e85bdc96
Udit Gupta <uditgupta222568@gmail.com>
2026-02-06 17:48:40 +0530

    Update solution for palindrome_number [javascript]

--- solutions/palindrome_number.js
+++ solutions/palindrome_number.js
@@ -1,8 +1,10 @@
 /*
  * Approach/Algorithm:
- * This solution checks if an integer is a palindrome by reversing the number and comparing it to the original. It first handles negative numbers by returning false. Then, it reverses the number by extracting digits in a loop and forming the reversed number.
- * Time Complexity: O(log10(n)), where n is the value of x, because we are processing each digit of the number.
- * Space Complexity: O(1), as we are using a constant amount of extra space.
+ * This solution checks if an integer is a palindrome by reversing the number and comparing it to the original.
+ * It first handles negative numbers by returning false, then reverses the number using a loop.
+
+ * Time Complexity: O(log10(n)) where n is the value of x.
+ * Space Complexity: O(1)
  */
 
 class Solution {
