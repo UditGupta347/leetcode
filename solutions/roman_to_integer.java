c279b7e590d9529db935bae3cc985e110f6c767c
Udit Gupta <uditgupta222568@gmail.com>
2026-02-06 17:29:06 +0530

    Update solution for roman_to_integer

--- solutions/roman_to_integer.java
+++ solutions/roman_to_integer.java
@@ -1,3 +1,11 @@
+/*
+ * Approach/Algorithm:
+ * Iterate through the string, for each character, check if it forms a subtractive combination with the next character.
+ * If it does, add the difference to the result and skip the next character. If it doesn't, add the value of the current character.
+ * Time Complexity: O(n), where n is the length of the string.
+ * Space Complexity: O(1), using a fixed amount of space for the mapping and variables.
+ */
+
 class Solution {
     public int romanToInt(String s) {
 
