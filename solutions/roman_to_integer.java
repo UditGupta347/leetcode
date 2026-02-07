ccd077e5834f17792107a999836472218fcd5598
Udit Gupta <uditgupta222568@gmail.com>
2026-02-07 23:25:14 +0530

    Add solution for roman_to_integer

---
+++ solutions/roman_to_integer.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int romanToInt(String s) {
+        int res = 0;
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
