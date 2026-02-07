e3cdfefabe1e80c1423b1011d4fedc56790e5148
Udit Gupta <uditgupta222568@gmail.com>
2026-02-07 22:51:13 +0530

    Add solution for roman_to_integer

---
+++ solutions/roman_to_integer.java
@@ -0,0 +1,19 @@
+/*
+ * Approach/Algorithm:
+ * This solution uses a HashMap to store the values of Roman numeral symbols and iterates through the string to convert it to an integer.
+ * It checks if the current symbol should be subtracted or added based on the next symbol.
+ *
+ * Time Complexity: O(n), where n is the length of the input string.
+ * Space Complexity: O(1), as the HashMap uses constant space.
+ */
+
+class Solution {
+    public int romanToInt(String s) {
+        int res = 0;
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
